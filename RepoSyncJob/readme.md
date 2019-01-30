c1e6a11c3b0da598e29f7eb509e51e3c353ea242
Nathan Novak <nanovak@microsoft.com>
2019-01-30 11:06:02 -0800

    Create task instructions, format cleanup

--- RepoSyncJob/readme.md
+++ RepoSyncJob/readme.md
@@ -1,11 +1,22 @@
 # RepoSyncJob
+
 This is a pair of .bat files meant to be run as a scheduled task to keep local repo clones in sync with what's in the master.
 
 The files are written for use on nanovak-home, but are easily configurable for another system (just update the paths).
 
 ## RunSync.bat
+
 This is the file that you run via Windows' task scheduler. It generates a current date/time variable and then kicks off GitPulls.bat
 
 ## GitPulls.bat
+
 1. Does a git pull for the specified repo
 2. Cleans up log files older than X days
+
+## Setup
+
+Create a Windows Scheduled Task
+
+* start at 12:15am
+* run cmd.exe
+* parameters: /c start /min D:\Repos\runsync.bat
--- RepoSyncJob/runsync.bat
+++ RepoSyncJob/runsync.bat
@@ -1,4 +1,5 @@
-echo off
+@echo off
+echo Updating DeviceConfig repo...echo off
 
 REM set log file name variable to include date/time of the job run
 set CUR_YYYY=%date:~10,4%
@@ -6,18 +7,12 @@ set CUR_MM=%date:~4,2%
 set CUR_DD=%date:~7,2%
 set CUR_HH=%time:~0,2%
 if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
-
 set CUR_NN=%time:~3,2%
 set CUR_SS=%time:~6,2%
 set CUR_MS=%time:~9,2%
-
 set LOGFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 
 REM do the needful
 D:\Repos\gitpulls.bat > D:\Repos\_syncjob\DeviceConfigs_%LOGFILENAME%.log 2>&1
-
-REM clean up the mess
-REM forfiles /p "D:\Repos\_syncjob" /s /m *.log /D -30 /C "cmd /c del @path" > D:\Repos\_syncjob\LogCleanup.log 2>&1
-
 
 exit
